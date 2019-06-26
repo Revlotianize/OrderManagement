51730b83d7d67ce3e8952b9da6ed4087b4bad3d0
Kartik <kartik.m@2adpro.com>
2019-06-26 18:32:07 +0530

    dashboard changess

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,6 +5,8 @@ import '../assets/css/styles.scss'
 import {Button, Icon, Card, Row, Col, Navbar, NavItem, CardTitle} from 'react-materialize';
 import Sidebar from './shared/sidebar/sidebar'
 import Header from './shared/header/header'
+import { Bar,Line,Pie } from 'react-chartjs-2';
+
 class Dashboard extends Component {
 
     constructor(props) {
@@ -12,15 +14,113 @@ class Dashboard extends Component {
         this.state = {
             responseCode: '',
             siteId:'',
-            loading:'initial'
+            loading:'initial',
+            chartData:{
+                options : {
+                    scales: {
+                        xAxes: [{
+                            barPercentage: 0.5,
+                            barThickness: 6,
+                            maxBarThickness: 1,
+                            minBarLength: 2,
+                            gridLines: {
+                                offsetGridLines: false
+                            }
+                        }]
+                    }
+                },
+                labels:['Length','Word','Breakdown','Data','Article'],
+                datasets:[
+                    {
+                        label:'Insights',
+                        data:[
+                            3,
+                            2,
+                            3,
+                            3,
+                            4
+                        ],
+                        backgroundColor:[
+                            'rgb(0,0,128)',
+                            'rgb(0,0,255)',
+                            'rgb(0,128,0)',
+                            'rgb(0,128,128)',
+                            'rgb(0,255,0)'
+                        ]
+                    }
+                ]
+
+            }
         }
     }
 
     render() {
         return(
-            <div>
-                 <Header/>
-                 <Sidebar/>
+            <div className="main-panel">
+                <h2>Pie Chart</h2>
+                <div className = "pie">
+                    <Pie
+                        data={this.state.chartData}
+                        options={{
+                            scales: {
+                                xAxes: [{
+                                    barPercentage: 0.5,
+                                    barThickness: 250,
+                                    maxBarThickness: 80,
+                                    minBarLength: 2,
+                                    gridLines: {
+                                        offsetGridLines: true
+                                    }
+                                }]
+                            }
+                        }}
+                        // width={100}
+                        // height={50}
+                        // options={{ maintainAspectRatio: true }}
+                    />
+                </div>
+                <div className = "linechart">
+                    <Line
+                        data={this.state.chartData}
+                        options={{
+                            scales: {
+                                xAxes: [{
+                                    barPercentage: 0.5,
+                                    barThickness: 250,
+                                    maxBarThickness: 80,
+                                    minBarLength: 2,
+                                    gridLines: {
+                                        offsetGridLines: true
+                                    }
+                                }]
+                            }
+                        }}
+                        // width={100}
+                        // height={50}
+                        // options={{ maintainAspectRatio: true }}
+                    />
+                </div>
+                <div className= "bar">
+                    <Bar className = "bar"
+                         data={this.state.chartData}
+                         options={{
+                             scales: {
+                                 xAxes: [{
+                                     barPercentage: 0.5,
+                                     barThickness: 250,
+                                     maxBarThickness: 80,
+                                     minBarLength: 2,
+                                     gridLines: {
+                                         offsetGridLines: true
+                                     }
+                                 }]
+                             }
+                         }}
+                        // width={100}
+                        // height={50}
+                        // options={{ maintainAspectRatio: true }}
+                    />
+                </div>
             </div>
         );
     }
